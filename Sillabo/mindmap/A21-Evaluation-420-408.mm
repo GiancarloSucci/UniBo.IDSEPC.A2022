01375d337472b552065e1bce93acaba4656da66a
GiancarloSucci <GiancarloSucci@users.noreply.github.com>
2022-06-05 00:14:15 +0300

    Initial commit

---
+++ Sillabo/mindmap/A21-Evaluation-420-408.mm
@@ -0,0 +1,239 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Evaluation" FOLDED="false" ID="ID_191153586" CREATED="1533282345765" MODIFIED="1547938963171" BACKGROUND_COLOR="#ccff00" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt" FORMAT="latexPatternFormat" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#0033cc" MAX_WIDTH="5.1 cm">
+<font SIZE="22" BOLD="true"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" ICON_SIZE="64.0 pt" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font NAME="Segoe Print" SIZE="22"/>
+<edge COLOR="#ffffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" ICON_SIZE="32.0 px" COLOR="#000000" BACKGROUND_COLOR="#ffffcc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="2.6 pt" SHAPE_VERTICAL_MARGIN="2.6 pt" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="18" BOLD="false" ITALIC="true"/>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" ICON_SIZE="28.0 px" COLOR="#000000" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="4"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" ICON_SIZE="24.0 px" COLOR="#000000" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" ICON_SIZE="24.0 px" COLOR="#111111" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="13"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" ICON_SIZE="24.0 px" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="12"/>
+<edge STYLE="bezier" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" ICON_SIZE="24.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" ICON_SIZE="16.0 px">
+<font SIZE="10"/>
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="25" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<edge COLOR="#ffffff"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      * To pass the course, students must obtain &gt;= 60% on the average of the exams
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Assignments" POSITION="right" ID="ID_273436263" CREATED="1533282345772" MODIFIED="1547938948779" HGAP_QUANTITY="58.26829726097857 pt" VSHIFT_QUANTITY="71.2682882134295 pt" BACKGROUND_COLOR="#cccc00">
+<icon BUILTIN="male2"/>
+<font BOLD="true"/>
+<edge COLOR="#007c7c"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      * Must be done individually
+    </p>
+    <p>
+      without collaboration
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="18%" ID="ID_588194298" CREATED="1533282345772" MODIFIED="1547972281673" HGAP_QUANTITY="59.25609634626952 pt" VSHIFT_QUANTITY="13.957315718995575 pt"/>
+</node>
+<node TEXT="Creative Team&#xa;Project" POSITION="right" ID="ID_495676614" CREATED="1533282345772" MODIFIED="1547938936684" HGAP_QUANTITY="70.30488307229803 pt" VSHIFT_QUANTITY="47.10365391605086 pt" BACKGROUND_COLOR="#00ff99">
+<icon BUILTIN="group"/>
+<font BOLD="true"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      * To be done in a team of
+    </p>
+    <p>
+      3 to 4 students maximum
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#009900"/>
+<node TEXT="20 %" ID="ID_744391648" CREATED="1533282345773" MODIFIED="1547551501885" HGAP_QUANTITY="62.25609625686255 pt" VSHIFT_QUANTITY="21.073168964417192 pt"/>
+</node>
+<node TEXT="Exams" POSITION="right" ID="ID_90823870" CREATED="1533282345773" MODIFIED="1547551533607" HGAP_QUANTITY="-288.8536462941072 pt" VSHIFT_QUANTITY="-110.21341604060427 pt">
+<icon BUILTIN="clock2"/>
+<edge COLOR="#ff0000"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>total weight:45%</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true"/>
+<node TEXT="Midterm" ID="ID_1201198400" CREATED="1533282345773" MODIFIED="1533287896644" HGAP_QUANTITY="-199.49389594237982 pt" VSHIFT_QUANTITY="-3.804879141662345 pt">
+<node TEXT="      20%" ID="ID_412150145" CREATED="1533282423063" MODIFIED="1547551527782" HGAP_QUANTITY="-143.49999530613434 pt" VSHIFT_QUANTITY="-5.999999821186066 pt"/>
+</node>
+<node TEXT="Final" ID="ID_1615683347" CREATED="1533282345773" MODIFIED="1533598005336" HGAP_QUANTITY="-176.73779892555802 pt" VSHIFT_QUANTITY="8.01219515123055 pt">
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>25%</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Theory" ID="ID_1721945531" CREATED="1533282432545" MODIFIED="1533287880837" HGAP_QUANTITY="-108.99999633431445 pt" VSHIFT_QUANTITY="-2.249999932944773 pt">
+<node TEXT="12.5%" ID="ID_489853020" CREATED="1533282457147" MODIFIED="1533597994828" HGAP_QUANTITY="-110.49999628961096 pt" VSHIFT_QUANTITY="-5.999999821186071 pt">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="Lab Exam" ID="ID_914954331" CREATED="1533282441683" MODIFIED="1533287885740" HGAP_QUANTITY="-128.49999575316917 pt" VSHIFT_QUANTITY="11.249999664723884 pt">
+<node TEXT="12.5%" ID="ID_1895446888" CREATED="1533282483898" MODIFIED="1533597996832" HGAP_QUANTITY="-128.49999575316917 pt" VSHIFT_QUANTITY="14.999999552965178 pt">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Surprise&#xa;Quizzes" POSITION="left" ID="ID_1832231319" CREATED="1533284948907" MODIFIED="1547938889963" HGAP_QUANTITY="56.74999872595076 pt" VSHIFT_QUANTITY="-56.99999830126767 pt" BACKGROUND_COLOR="#ff9999">
+<icon BUILTIN="clock"/>
+<font BOLD="true"/>
+<edge COLOR="#7c0000"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      * Can be given any time,
+    </p>
+    <p>
+      any place (lecture/lab)
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="   7%" ID="ID_76897171" CREATED="1533285166828" MODIFIED="1547551480978" HGAP_QUANTITY="77.749998100102 pt" VSHIFT_QUANTITY="3.552713678800501E-15 pt" TEXT_ALIGN="RIGHT"/>
+</node>
+<node TEXT="In-Class&#xa;Labs &amp; activities" POSITION="left" ID="ID_321937343" CREATED="1533285438191" MODIFIED="1547938963156" HGAP_QUANTITY="-410.49998734891454 pt" VSHIFT_QUANTITY="44.24999868124728 pt" BACKGROUND_COLOR="#00ffcc" STYLE="rectangle" SHAPE_HORIZONTAL_MARGIN="2.6 pt" SHAPE_VERTICAL_MARGIN="2.6 pt">
+<icon BUILTIN="female1"/>
+<font BOLD="true"/>
+<edge COLOR="#0000ff"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      * Consists of closed-book programming exercises
+    </p>
+    <p>
+      and other graded activities
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="10%" ID="ID_491325183" CREATED="1533285467845" MODIFIED="1547972259459" HGAP_QUANTITY="-308.4999903887513 pt" VSHIFT_QUANTITY="6.749999798834326 pt"/>
+</node>
+</node>
+</map>
